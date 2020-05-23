e2f9f635b9fa4b42ff00922e793982630cf2efd8
Srinivasulu Kanta <srinivasulu.kanta@gmail.com>
2020-05-23 19:49:13 +0530

    feat: Added commit lint rule for commit body
    This commit has the addition of rule for commit body maximum length 72
    chars

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [2, "never", ["start-case", "pascal-case"]],
+    "body-max-line-length": [2, "always", 72],
   },
   ignores: [],
 };
